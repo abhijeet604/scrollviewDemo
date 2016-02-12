ec55671e2389af3140bad380552fa774f881024e
iBoss <iboss@ibosss-mac-mini.local>
2016-02-12 15:24:54 +0530

    Initial Commit

---
+++ scrollviewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scrollviewDemo
+//
+//  Created by iBoss on 12/02/16.
+//  Copyright © 2016 iBoss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ scrollviewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  scrollviewDemo
+//
+//  Created by iBoss on 12/02/16.
+//  Copyright © 2016 iBoss. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
